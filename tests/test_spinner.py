d23da16d72b5985172e71a966649d26bc8696ed8
Denis <fbjorn@users.noreply.github.com>
2022-10-17 18:20:43 +0300

    Bump version and add more tests

--- tests/test_spinner.py
+++ tests/test_spinner.py
@@ -1,6 +1,7 @@
 from PyQt5.QtWidgets import QWidget
 
 from pyqtspinner import WaitingSpinner
+from pyqtspinner.configurator import SpinnerConfigurator
 
 
 def test_spinner_creation(qtbot):
@@ -9,3 +10,9 @@ def test_spinner_creation(qtbot):
 
     spinner.start()
     spinner.stop()
+
+
+def test_randomization_not_raising_value_errors(qtbot):
+    configurator = SpinnerConfigurator()
+
+    configurator._randomize()
